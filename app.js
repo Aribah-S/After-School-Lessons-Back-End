bff831b1cba4f8aff459b8108010b3f86d6b6b7d
Aribah <aribahshasmeen@gmail.com>
2024-12-04 23:46:16 +0400

    added CORS functionality

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const app = express();
 
 const port = 3005;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+
+  next();
+});
+
 const url =
   "mongodb+srv://aribahshasmeen:fullstack@cluster0.ghnuw.mongodb.net/";
 const dbName = "afterSchoolLessons";
