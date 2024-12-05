54f6743342685cb8ffcfc21415bba3059a5e5740
Aribah <aribahshasmeen@gmail.com>
2024-12-05 11:44:53 +0400

    installed cors module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
 
 var path = require("path");
 var fs = require("fs");
@@ -8,20 +9,20 @@ const app = express();
 
 const port = 3005;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, HEAD, OPTIONS, POST, PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Origin",
+      "Accept",
+      "X-Requested-With",
+    ],
+    credentials: true,
+  })
+);
 
 const url =
   "mongodb+srv://aribahshasmeen:fullstack@cluster0.ghnuw.mongodb.net/";
